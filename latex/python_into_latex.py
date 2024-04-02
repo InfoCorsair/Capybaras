d2c902f828c716bbd91e6f4b8e5098582d0046c8
RipperDoc01 <115053963+InfoCorsair@users.noreply.github.com>
2024-04-02 15:42:25 +0000

    Updated latex, mongo, and recipe folders to match code in csLab

--- latex/python_into_latex.py
+++ latex/python_into_latex.py
@@ -1,27 +1,26 @@
 title = input("Enter Title: ")
 
-
-
 with open('test_output.tex', 'w') as f:
   f.write('\\title{CapyCookin}\n')
-  f.write('\\begin{center}\n')
+  f.write('\\documentclass[a4paper,12pt]{article}\n')
+  f.write('\\begin{document}\n')
   f.write('\maketitle\n')
   
-  f.write('\\textbf{' + title + '}\n') 
+  f.write('\\textbf{' + title + '}\\newline\n') 
 
   ingredient = ""
   while (1):
     ingredient = input("Ingredient: ")
     if ingredient == 'STOP':
       break
-    f.write('\\textbf{' + ingredient + '}\n')
+    f.write('\\textbf{' + ingredient + '}\\newline\n')
 
 
   servings = input("Enter number of servings: ")
-  f.write('\\textbf{' + servings + '}\n')
+  f.write('\\textbf{' + servings + '}\\newline\n')
 
   recipe = input("Enter your recipe: ")
-  f.write('\\textbf{' + recipe + '}\n')
+  f.write('\\textbf{' + recipe + '}\\newline\n')
 
 
-  f.write('\end{center}\n')
+  f.write('\end{document}\n')
